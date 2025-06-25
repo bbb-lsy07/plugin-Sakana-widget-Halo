8da6add843768b199637f567fe18d2b279d2f94d
bbb-lsy07 <pitas_hadron0w@icloud.com>
2025-06-25 08:23:01 +0800

    Update index.ts

--- ui/src/index.ts
+++ ui/src/index.ts
@@ -1,5 +1,6 @@
 import { definePlugin } from '@halo-dev/console-shared'
 import HomeView from './views/HomeView.vue'
+import SettingsView from './views/SettingsView.vue'
 import { IconPlug } from '@halo-dev/components'
 import { markRaw } from 'vue'
 
@@ -20,6 +21,24 @@ export default definePlugin({
             group: '示例分组',
             icon: markRaw(IconPlug),
             priority: 0,
+          },
+        },
+      },
+    },
+    {
+      parentName: 'Root',
+      route: {
+        path: '/settings',
+        name: 'Settings',
+        component: SettingsView,
+        meta: {
+          title: '插件设置',
+          searchable: true,
+          menu: {
+            name: '插件设置',
+            group: '示例分组',
+            icon: markRaw(IconPlug),
+            priority: 1,
           },
         },
       },
